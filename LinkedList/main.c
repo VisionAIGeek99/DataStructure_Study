e0fc805663a1eb2a9e4dafcbef2bacd4f70e7d7d
kmw2622 <kmw2622@naver.com>
2023-05-13 19:19:38 +0900

    LinkedList add

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -1,5 +1,14 @@
 ﻿#include <stdio.h>
-#include "ArrayList.h"
+//#include "ArrayList.h"
+#include "LinkedList.h"
+
+int WhoIsPrecede(int d1, int d2)
+{
+	if (d1 < d2)
+		return 0;
+	else
+		return 1;
+}
 
 int main()
 {
@@ -7,9 +16,13 @@ int main()
 	int data;
 	ListInit(&list);
 
+	SetSortRule(&list, WhoIsPrecede);
+
 	LInsert(&list, 11); LInsert(&list, 22);
 	LInsert(&list, 33); LInsert(&list, 44);
 	LInsert(&list, 55); LInsert(&list, 66);
+
+	printf("현재 데이터의 수: %d\n", LCount(&list));
 
 	if (LFirst(&list, &data))
 	{
@@ -19,4 +32,22 @@ int main()
 			printf("%d ", data);
 	}
 	printf("\n\n");
+	if (LFirst(&list, &data))
+	{
+		if (data == 22)
+			LRemove(&list);
+		while (LNext(&list, &data)) {
+			if (data == 22)
+				LRemove(&list);
+		}
+	}
+	printf("현재 데이터의 수: %d\n", LCount(&list));
+
+	if (LFirst(&list, &data))
+	{
+		printf("%d ", data);
+
+		while (LNext(&list, &data))
+			printf("%d ", data);
+	}
 }
